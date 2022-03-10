63729e486bd9afe9420b9bf8e1bbcd11ded37695
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-03-10 01:13:59 -0400

    Mostrar el descuento que corresponde a 3000

--- src/porcentaje_descuento.js
+++ src/porcentaje_descuento.js
@@ -1,6 +1,9 @@
 function porcentaje_descuento(cantidad, precio) {
     var descuento = 0;
-    if(cantidad >= 1000){
+    if(cantidad >= 3000){
+        descuento = (precio*5)/100;
+    }
+    else if(cantidad >= 1000){
         descuento = (precio*3)/100;
     }
     return descuento;
