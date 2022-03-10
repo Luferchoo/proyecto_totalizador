25b402f09d5f2d285f552cefe21e6fa548dc0594
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-03-09 21:04:16 -0400

    Ingresar el precio por item y mostrar en pantalla lo ingresado

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 //import sumar from "./sumador";
 
 const items = document.querySelector("#items-numero");
+const precio = document.querySelector("#precio-numero");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,5 +9,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const items_numero = Number.parseInt(items.value);
-  div.innerHTML = "<p>" + "Cantidad de items:      " + items_numero + "</p>";
+  const precio_numero = Number.parseFloat(precio.value);
+  div.innerHTML = "<p>" + "Cantidad de items:      " + items_numero + "<br/>Precio por Item:       " + precio_numero + "</p>";
 });
