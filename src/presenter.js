4c3d12cb53565cf70bb1c0ade43bf4f04281e54b
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-03-09 22:17:00 -0400

    Seleccionar estado y mostrar elección

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,8 @@
 
 const items = document.querySelector("#items-numero");
 const precio = document.querySelector("#precio-numero");
+const estado = document.querySelector("#estados");
+
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
@@ -10,5 +12,6 @@ form.addEventListener("submit", (event) => {
 
   const items_numero = Number.parseInt(items.value);
   const precio_numero = Number.parseFloat(precio.value);
-  div.innerHTML = "<p>" + "Cantidad de items:      " + items_numero + "<br/>Precio por Item:       " + precio_numero + "</p>";
+  const estado_string = estado.value;
+  div.innerHTML = "<p>" + "Cantidad de items:      " + items_numero + "<br/>Precio por Item:       " + precio_numero + "<br/>estado:       " + estado_string + "</p>";
 });
